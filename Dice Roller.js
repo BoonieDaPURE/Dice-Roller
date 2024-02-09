db28a0b7328619e406e0c97d125b3a6cc637e5d0
BoonieDaPURE <jacostaprof@gmail.com>
2024-02-09 17:25:09 -0600

    Version 2.2: notation changes

--- Dice Roller.js
+++ Dice Roller.js
@@ -1,18 +1,27 @@
-//creates variables for the input fields and result value
+/*
+program start ->
+creates variables for the input fields and result value
+*/
 const amount = document.getElementById("amount-el");
 const sides = document.getElementById("sides-el");
 const result = document.getElementById("result-el");
 
-//fetches local storage key/values and updates input fields and result value
+/*
+program start ->
+---fetches local storage key/values 
+---updates input fields and result value
+*/
 amount.value = localStorage.getItem("amount")
 sides.value = localStorage.getItem("sides")
 result.textContent = localStorage.getItem("result")
 
-//ROLL BUTTON: 
-//on click -> 
-//---executes diceRoller() 
-//---modifies result text
-//---stores field and result data in local storage
+/*
+ROLL BUTTON: 
+on click -> 
+---executes diceRoller() 
+---modifies result text
+---stores field and result data in local storage
+*/
 document.getElementById("roll-button").addEventListener("click", function () {
     let amountEl = document.getElementById("amount-el");
     let sidesEl = document.getElementById("sides-el");
@@ -20,10 +29,12 @@ document.getElementById("roll-button").addEventListener("click", function () {
     localStorage.setItem("amount", amountEl.value);
     localStorage.setItem("sides", sidesEl.value);
     localStorage.setItem("result", result.textContent);
-}
-);
+});
 
-//rolls dice and returns the summed value
+/*
+called by ROLL BUTTON ->
+rolls dice and returns the summed value
+*/
 function diceRoller(amount,sides) {
     if (amount < 1) {
         console.log(amount)
@@ -39,9 +50,11 @@ function diceRoller(amount,sides) {
     }
 };
 
-//CLEAR BUTTON: 
-//on click -> 
-//---clears input fields, result value, and local storage key/values
+/*
+CLEAR BUTTON: 
+on click -> 
+---clears input fields, result value, and local storage key/values
+*/
 document.getElementById("clear-button").addEventListener("click", function() {
     amount.value = null;
     sides.value = null;
@@ -49,5 +62,4 @@ document.getElementById("clear-button").addEventListener("click", function() {
     localStorage.setItem("amount", amount.value);
     localStorage.setItem("sides", sides.value);
     localStorage.setItem("result", result.textContent);
-}
-);
+});
