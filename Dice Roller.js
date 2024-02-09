00b475a03a2fb4e6b22d7d250159a324a06f7fbd
BoonieDaPURE <jacostaprof@gmail.com>
2024-02-09 14:12:53 -0600

    Version 1.8

--- Dice Roller.js
+++ Dice Roller.js
@@ -1,22 +1,22 @@
-let resultEl = document.getElementById("result-el") 
-
-function diceRoller(amount,faces) {
-    if (amount < 1) {
-        return "No dice detected"
-    }
-    if (amount % 1 != 0 || faces % 1 !=0) {
-        return "Broken dice detected"
-    }
-    if (amount === 1) {
-        return Math.floor(Math.random() * faces) + 1
-    } else {
-        return Math.floor(Math.random() * faces) + 1 + diceRoller(amount - 1, faces)
-    }
-
+function rollButton() {
+    let resultEl = document.getElementById("result-el") 
+    let amountEl = document.getElementById("amount-el").value
+    let sidesEl = document.getElementById("sides-el").value
+    resultEl.textContent = "Result: " + diceRoller(amountEl,sidesEl)
 }
 
-function rollButton() {
-    let amountEl = document.getElementById("amount-el").value
-    let facesEl = document.getElementById("faces-el").value
-    resultEl.textContent = "Result: " + diceRoller(amountEl,facesEl)
+function diceRoller(amount,sides) {
+    if (amount < 1) {
+        console.log(amount)
+        return "No dice detected"
+    }
+    if (amount % 1 != 0 || sides % 1 !=0) {
+        return "Broken dice detected"
+    }
+    if (amount == 1) {
+        return Math.floor(Math.random() * sides) + 1
+    } else {
+        return Math.floor(Math.random() * sides) + 1 + diceRoller(amount - 1, sides)
+    }
+
 }
